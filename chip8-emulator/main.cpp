c9bc6af2289067bd51a43eb27c3dd97ed6f12ebf
Alihan Baysal <baysal.alihan@outlook.com>
2025-08-20 13:08:30 -0700

    Initial Commit

---
+++ chip8-emulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chip8-emulator
+//
+//  Created by Alihan Baysal on 8/20/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
